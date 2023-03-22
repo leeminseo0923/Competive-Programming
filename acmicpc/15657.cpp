82ae60499612cb723cabfba5ff7c6bd7aad8e6a2
leeminseo0923 <64004511+leeminseo0923@users.noreply.github.com>
2023-03-22 12:39:57 +0900

    15657

---
+++ acmicpc/15657.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+
+using namespace std;
+
+int n, m;
+bool visited[MAX] = {
+    0,
+};
+int list[MAX] = {
+    0,
+};
+
+int number[MAX] = {
+    0,
+};
+
+void dfs(int count, int num)
+{
+    if (count == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << list[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = num; i < n; i++)
+    {
+
+        list[count] = number[i];
+        dfs(count + 1, i);
+        visited[i] = false;
+    }
+}
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> number[i];
+    }
+    sort(number, number + n);
+    dfs(0, 0);
+}
